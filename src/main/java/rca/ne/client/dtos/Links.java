1e3e83083660a779c591de89e74afbd51a7bee43
Shallon Kobusinge <kobshallon041@gmail.com>
2022-07-11 15:02:53 +0300

    feat: json pattern

--- src/main/java/rca/ne/client/dtos/Links.java
+++ src/main/java/rca/ne/client/dtos/Links.java
@@ -1,9 +1,11 @@
 package rca.ne.client.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -15,6 +17,8 @@ public class Links {
 
     private Long id;
     private String linkName;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime totalElapsedTime;
     private String numberOfKilobytesDownloaded;
     private Website website;
--- src/main/java/rca/ne/client/dtos/Website.java
+++ src/main/java/rca/ne/client/dtos/Website.java
@@ -1,9 +1,11 @@
 package rca.ne.client.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -12,9 +14,14 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Website {
-
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     LocalDateTime startDate;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     LocalDateTime endDate;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     LocalDateTime totalElapsedTime;
     String numberOfKilobytesDownloaded;
     private Long id;
