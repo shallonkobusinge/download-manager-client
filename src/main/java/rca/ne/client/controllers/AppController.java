af05eb3ba5de747615bee2e237799be2c4d8b94f
Shallon Kobusinge <kobshallon041@gmail.com>
2022-07-11 01:33:28 -0700

    feat: views

---
+++ src/main/java/rca/ne/client/controllers/AppController.java
@@ -0,0 +1,26 @@
+package rca.ne.client.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+@RequestMapping("/")
+public class AppController {
+    @GetMapping
+    public String mainEntry() {
+            return "Download";
+    }
+    @PostMapping("/download")
+    public String downloadPage(String url) {
+        System.out.println("Url "+ url);
+        RestTemplate restTemplate = new RestTemplate();
+        String URL = "http://localhost:5500/api/v1/websites";
+        ResponseEntity<String> response = restTemplate.postForEntity(URL,url, String.class);
+        System.out.println("Response "+ response.getBody());
+        return "";
+    }
+}
